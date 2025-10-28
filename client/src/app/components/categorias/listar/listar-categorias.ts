d4647313cc9bb07ca700e0cb0cdc98e5374b2c18
Victor R. Silva <victorr.silva005@gmail.com>
2025-10-28 13:53:26 -0300

    Conexão com api e listagem de categorias

---
+++ client/src/app/components/categorias/listar/listar-categorias.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { CategoriaService } from '../categoria.service';
+import { AsyncPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-listar-categorias',
+  imports: [MatButtonModule,MatIconModule,MatCardModule,RouterLink,AsyncPipe],
+  templateUrl: './listar-categorias.html',
+})
+export class ListarCategorias {
+  protected readonly categoriaService = inject(CategoriaService);
+
+  protected readonly categorias$ = this.categoriaService.selecionarTodas();
+}
