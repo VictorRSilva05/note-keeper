1fc5fa57a0055c422588b24d2243a4680e140c90
Victor R. Silva <victorr.silva005@gmail.com>
2025-10-29 14:06:45 -0300

    Resolver detalhes de categoria

--- client/src/app/components/categorias/categoria.routes.ts
+++ client/src/app/components/categorias/categoria.routes.ts
@@ -1,22 +1,50 @@
-import { ResolveFn, Routes } from '@angular/router';
-import { ListarCategorias } from './listar/listar-categorias';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, ResolveFn, Routes } from '@angular/router';
+
 import { CadastrarCategoria } from './cadastrar/cadastrar-categoria';
-import { EditarCategoria } from './editar/editar-categoria';
-import { ExcluirCategoria } from './excluir/excluir-categoria';
 import { ListagemCategoriasModel } from './categoria.models';
 import { CategoriaService } from './categoria.service';
-import { inject } from '@angular/core';
-import { delay } from 'rxjs';
+import { EditarCategoria } from './editar/editar-categoria';
+import { ExcluirCategoria } from './excluir/excluir-categoria';
+import { ListarCategorias } from './listar/listar-categorias';
 
 const listagemCategoriasResolver: ResolveFn<ListagemCategoriasModel[]> = () => {
   const categoriaService = inject(CategoriaService);
 
-  return categoriaService.selecionarTodas().pipe(delay(3000));
-}
+  return categoriaService.selecionarTodas();
+};
+
+const detalhesCategoriaResolver = (route: ActivatedRouteSnapshot) => {
+  const categoriaService = inject(CategoriaService);
+
+  if (!route.paramMap.has('id')) throw new Error('O parâmetro id não foi fornecido.');
+
+  const categoriaId = route.paramMap.get('id')!;
+
+  return categoriaService.selecionarPorId(categoriaId);
+};
 
 export const categoriaRoutes: Routes = [
-  {path: '', component: ListarCategorias, resolve: {categorias: listagemCategoriasResolver} },
-  {path: 'cadastrar', component: CadastrarCategoria},
-  {path: 'editar/:id', component: EditarCategoria},
-  {path: 'excluir/:id', component: ExcluirCategoria}
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: ListarCategorias,
+        resolve: { categorias: listagemCategoriasResolver },
+      },
+      { path: 'cadastrar', component: CadastrarCategoria },
+      {
+        path: 'editar/:id',
+        component: EditarCategoria,
+        resolve: { categoria: detalhesCategoriaResolver },
+      },
+      {
+        path: 'excluir/:id',
+        component: ExcluirCategoria,
+        resolve: { categoria: detalhesCategoriaResolver },
+      },
+    ],
+    providers: [CategoriaService]
+  },
 ];
