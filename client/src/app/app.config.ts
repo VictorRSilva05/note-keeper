174fc291611821af7299522a1f3a200174d966b8
Victor R. Silva <victorr.silva005@gmail.com>
2025-10-29 10:50:50 -0300

    Serviço de notificação e estillização

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -5,6 +5,7 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
+import { provideNotifications } from './components/shared/notificacao/notificacao.provider';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
@@ -23,7 +24,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-
     provideHttpClient(),
+
+    provideNotifications(),
   ],
 };
