cdf1b15e1b3d330723540f55a88e08426fd5f6ca
Victor R. Silva <victorr.silva005@gmail.com>
2025-10-29 14:03:59 -0300

    resolver de listagem categoria

--- client/src/app/components/categorias/categoria.routes.ts
+++ client/src/app/components/categorias/categoria.routes.ts
@@ -1,11 +1,21 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 import { ListarCategorias } from './listar/listar-categorias';
 import { CadastrarCategoria } from './cadastrar/cadastrar-categoria';
 import { EditarCategoria } from './editar/editar-categoria';
 import { ExcluirCategoria } from './excluir/excluir-categoria';
+import { ListagemCategoriasModel } from './categoria.models';
+import { CategoriaService } from './categoria.service';
+import { inject } from '@angular/core';
+import { delay } from 'rxjs';
+
+const listagemCategoriasResolver: ResolveFn<ListagemCategoriasModel[]> = () => {
+  const categoriaService = inject(CategoriaService);
+
+  return categoriaService.selecionarTodas().pipe(delay(3000));
+}
 
 export const categoriaRoutes: Routes = [
-  {path: '', component: ListarCategorias },
+  {path: '', component: ListarCategorias, resolve: {categorias: listagemCategoriasResolver} },
   {path: 'cadastrar', component: CadastrarCategoria},
   {path: 'editar/:id', component: EditarCategoria},
   {path: 'excluir/:id', component: ExcluirCategoria}
--- client/src/app/components/categorias/listar/listar-categorias.ts
+++ client/src/app/components/categorias/listar/listar-categorias.ts
@@ -3,8 +3,10 @@ import { CategoriaService } from '../categoria.service';
 import { AsyncPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { delay, filter, map } from 'rxjs';
+import { ListagemCategoriasModel } from '../categoria.models';
 
 @Component({
   selector: 'app-listar-categorias',
@@ -12,7 +14,11 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './listar-categorias.html',
 })
 export class ListarCategorias {
+  protected readonly route = inject(ActivatedRoute)
   protected readonly categoriaService = inject(CategoriaService);
 
-  protected readonly categorias$ = this.categoriaService.selecionarTodas();
+  protected readonly categorias$ = this.route.data.pipe(
+    filter(data=> data['categorias']),
+    map(data => data['categorias'] as ListagemCategoriasModel)
+  );
 }
