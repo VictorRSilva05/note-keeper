5cd3e1255ae74a79f87cd0bdfd95d21f5e405dfa
Victor R. Silva <victorr.silva005@gmail.com>
2025-10-31 14:04:57 -0300

    Corrigido listagem de categorias

--- client/src/app/components/categorias/listar/listar-categorias.ts
+++ client/src/app/components/categorias/listar/listar-categorias.ts
@@ -1,24 +1,26 @@
-import { Component, inject } from '@angular/core';
-import { CategoriaService } from '../categoria.service';
+import { filter, map } from 'rxjs';
+
 import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { delay, filter, map } from 'rxjs';
+
 import { ListagemCategoriasModel } from '../categoria.models';
+import { CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-listar-categorias',
-  imports: [MatButtonModule,MatIconModule,MatCardModule,RouterLink,AsyncPipe],
+  imports: [MatButtonModule, MatIconModule, MatCardModule, RouterLink, AsyncPipe],
   templateUrl: './listar-categorias.html',
 })
 export class ListarCategorias {
-  protected readonly route = inject(ActivatedRoute)
+  protected readonly route = inject(ActivatedRoute);
   protected readonly categoriaService = inject(CategoriaService);
 
   protected readonly categorias$ = this.route.data.pipe(
-    filter(data=> data['categorias']),
-    map(data => data['categorias'] as ListagemCategoriasModel)
+    filter((data) => data['categorias']),
+    map((data) => data['categorias'] as ListagemCategoriasModel[]),
   );
 }
