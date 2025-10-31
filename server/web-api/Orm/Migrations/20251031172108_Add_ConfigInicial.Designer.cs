8ae54bbb2e8f77db26b295b49aae8b690d8ad1ec
Victor R. Silva <victorr.silva005@gmail.com>
2025-10-31 14:27:14 -0300

    Alterado server

--- server/web-api/Orm/Migrations/20251031172108_Add_ConfigInicial.Designer.cs
+++ server/web-api/Orm/Migrations/20251031172108_Add_ConfigInicial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NoteKeeper.WebApi.Orm;
 
@@ -11,9 +12,11 @@ using NoteKeeper.WebApi.Orm;
 namespace NoteKeeper.WebApi.Orm.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251031172108_Add_ConfigInicial")]
+    partial class Add_ConfigInicial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
