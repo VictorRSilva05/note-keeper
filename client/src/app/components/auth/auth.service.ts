d5180892488e5ad6edf4c7b4f5051b75b931648c
Victor R. Silva <victorr.silva005@gmail.com>
2025-11-04 13:49:14 -0300

    Restauração de sessão do LocalStorage

--- client/src/app/components/auth/auth.service.ts
+++ client/src/app/components/auth/auth.service.ts
@@ -1,22 +1,47 @@
-import { BehaviorSubject, Observable, shareReplay, tap } from 'rxjs';
+import {
+    BehaviorSubject, defer, distinctUntilChanged, merge, Observable, of, shareReplay, skip, tap
+} from 'rxjs';
 
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 import { environment } from '../../../environments/environment';
-import { AccessTokenModel, LoginModel, RegistroModel } from './auth.models';
 import { obterOpcoesHeaderAutorizacao } from '../../util/obter-opcoes-header-autorizacao';
+import { AccessTokenModel, LoginModel, RegistroModel } from './auth.models';
 
 @Injectable()
 export class AuthService {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = environment.apiUrl + '/auth';
+  private readonly chaveAccessToken: string = 'notekeeper:access-token';
 
   public readonly accessTokenSubject$ = new BehaviorSubject<AccessTokenModel | undefined>(
     undefined,
   );
 
-  public readonly accessToken$ = this.accessTokenSubject$.pipe(
+  public readonly accessTokenArmazenado$ = defer(() => {
+    const accessToken = this.obterAccessToken();
+
+    if (!accessToken) return of(undefined);
+
+    const valido = accessToken.expiracao > new Date(); // DateTime.Now
+
+    if (!valido) return of(undefined);
+
+    return of(accessToken);
+  });
+
+  public readonly accessToken$ = merge(
+    this.accessTokenArmazenado$,
+    this.accessTokenSubject$.pipe(skip(1)),
+  ).pipe(
+    distinctUntilChanged((a, b) => a === b),
+    tap((accessToken) => {
+      if (accessToken) this.salvarAccessToken(accessToken);
+      else this.limparAccessToken();
+
+      this.accessTokenSubject$.next(accessToken);
+    }),
     shareReplay({ bufferSize: 1, refCount: true }),
   );
 
@@ -46,5 +71,23 @@ export class AuthService {
         obterOpcoesHeaderAutorizacao(this.accessTokenSubject$.getValue()),
       )
       .pipe(tap(() => this.accessTokenSubject$.next(undefined)));
+  }
+
+  private salvarAccessToken(token: AccessTokenModel): void {
+    const jsonString = JSON.stringify(token);
+
+    localStorage.setItem(this.chaveAccessToken, jsonString);
+  }
+
+  private limparAccessToken(): void {
+    localStorage.removeItem(this.chaveAccessToken);
+  }
+
+  private obterAccessToken(): AccessTokenModel | undefined {
+    const jsonString = localStorage.getItem(this.chaveAccessToken);
+
+    if (!jsonString) return undefined;
+
+    return JSON.parse(jsonString);
   }
 }
